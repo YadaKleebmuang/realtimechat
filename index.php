3d28c50d8bd0769121c48f3c4cbbe98797db6627
sirilak <sirilakphaethisng47@gmail.com>
2025-03-12 21:54:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>แชท</title>
+    </head>
+<body style="text-align: center;">
+    <br>
+    <form action="">
+        <input type="text" name="name" placeholder="ชื่อ"><br>
+        <textarea name="txt"  placeholder="ข้อความ" cols="30" rows="10"></textarea><br>
+        <button type="button">ส่งข้อความ</button>
+    </form>
+
+    <div id="show">
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</body>
+</html>
